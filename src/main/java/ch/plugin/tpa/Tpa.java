a07df481fa790cc25c25b492fee44bd56befd8eb
Joel-IT <joelkosmos@outlook.de>
2024-12-07 10:04:24 +0100

    dev

--- src/main/java/ch/plugin/tpa/Tpa.java
+++ src/main/java/ch/plugin/tpa/Tpa.java
@@ -2,6 +2,7 @@ package ch.plugin.tpa;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -15,6 +16,7 @@ import java.util.UUID;
 public class Tpa extends JavaPlugin {
 
     private final HashMap<UUID, UUID> tpaRequests = new HashMap<>();
+    private Location spawnLocation;
 
     @Override
     public void onEnable() {
@@ -22,6 +24,8 @@ public class Tpa extends JavaPlugin {
         getCommand("tpa").setExecutor(new CommandHandler());
         getCommand("tpaccept").setExecutor(new CommandHandler());
         getCommand("tpdeny").setExecutor(new CommandHandler());
+        getCommand("spawn").setExecutor(new CommandHandler());
+        getCommand("setspawn").setExecutor(new CommandHandler()); // Registrierung des setspawn-Befehls
         getLogger().info("TPA Plugin wurde aktiviert!");
     }
 
@@ -43,56 +47,89 @@ public class Tpa extends JavaPlugin {
 
             switch (command.getName().toLowerCase()) {
                 case "tpa":
-                    if (args.length != 1) {
-                        player.sendMessage(ChatColor.YELLOW + "Benutzung: /tpa <spieler>");
-                        return false;
-                    }
-
-                    Player target = Bukkit.getPlayer(args[0]);
-                    if (target == null || target.equals(player)) {
-                        player.sendMessage(ChatColor.RED + "Spieler nicht gefunden oder ungültig.");
-                        return true;
-                    }
-
-                    tpaRequests.put(target.getUniqueId(), player.getUniqueId());
-                    target.sendMessage(ChatColor.GREEN + player.getName() + " hat dir eine Teleport-Anfrage gesendet. Tippe /tpaccept um zu akzeptieren oder /tpdeny um abzulehnen.");
-                    player.sendMessage(ChatColor.GREEN + "Teleport-Anfrage an " + target.getName() + " gesendet.");
-                    break;
-
+                    return handleTpaCommand(player, args);
                 case "tpaccept":
-                    if (!tpaRequests.containsKey(player.getUniqueId())) {
-                        player.sendMessage(ChatColor.RED + "Du hast keine ausstehenden Teleport-Anfragen.");
-                        return true;
-                    }
-
-                    UUID requesterId = tpaRequests.get(player.getUniqueId());
-                    Player requester = Bukkit.getPlayer(requesterId);
-                    if (requester == null) {
-                        player.sendMessage(ChatColor.RED + "Der Spieler, der die Anfrage gesendet hat, ist nicht mehr online.");
-                        tpaRequests.remove(player.getUniqueId());
-                        return true;
-                    }
-
-                    tpaRequests.remove(player.getUniqueId());
-                    initiateTeleport(requester, player);
-                    break;
-
+                    return handleTpAcceptCommand(player);
                 case "tpdeny":
-                    if (!tpaRequests.containsKey(player.getUniqueId())) {
-                        player.sendMessage(ChatColor.RED + "Du hast keine ausstehenden Teleport-Anfragen.");
-                        return true;
-                    }
-
-                    UUID deniedRequesterId = tpaRequests.get(player.getUniqueId());
-                    Player deniedRequester = Bukkit.getPlayer(deniedRequesterId);
-                    if (deniedRequester != null) {
-                        deniedRequester.sendMessage(ChatColor.RED + player.getName() + " hat deine Teleport-Anfrage abgelehnt.");
-                    }
-
-                    player.sendMessage(ChatColor.YELLOW + "Teleport-Anfrage abgelehnt.");
-                    tpaRequests.remove(player.getUniqueId());
-                    break;
+                    return handleTpDenyCommand(player);
+                case "spawn":
+                    return handleSpawnCommand(player);
+                case "setspawn":
+                    return handleSetSpawnCommand(player); // Neuer Befehl um den Spawn zu setzen
             }
+            return true;
+        }
+
+        private boolean handleTpaCommand(Player player, String[] args) {
+            if (args.length != 1) {
+                player.sendMessage(ChatColor.YELLOW + "Benutzung: /tpa <spieler>");
+                return false;
+            }
+
+            Player target = Bukkit.getPlayer(args[0]);
+            if (target == null || target.equals(player)) {
+                player.sendMessage(ChatColor.RED + "Spieler nicht gefunden oder ungültig.");
+                return true;
+            }
+
+            tpaRequests.put(target.getUniqueId(), player.getUniqueId());
+            target.sendMessage(ChatColor.GREEN + player.getName() + " hat dir eine Teleport-Anfrage gesendet. Tippe /tpaccept um zu akzeptieren oder /tpdeny um abzulehnen.");
+            player.sendMessage(ChatColor.GREEN + "Teleport-Anfrage an " + target.getName() + " gesendet.");
+            return true;
+        }
+
+        private boolean handleTpAcceptCommand(Player player) {
+            if (!tpaRequests.containsKey(player.getUniqueId())) {
+                player.sendMessage(ChatColor.RED + "Du hast keine ausstehenden Teleport-Anfragen.");
+                return true;
+            }
+
+            UUID requesterId = tpaRequests.get(player.getUniqueId());
+            Player requester = Bukkit.getPlayer(requesterId);
+            if (requester == null) {
+                player.sendMessage(ChatColor.RED + "Der Spieler, der die Anfrage gesendet hat, ist nicht mehr online.");
+                tpaRequests.remove(player.getUniqueId());
+                return true;
+            }
+
+            tpaRequests.remove(player.getUniqueId());
+            initiateTeleport(requester, player);
+            return true;
+        }
+
+        private boolean handleTpDenyCommand(Player player) {
+            if (!tpaRequests.containsKey(player.getUniqueId())) {
+                player.sendMessage(ChatColor.RED + "Du hast keine ausstehenden Teleport-Anfragen.");
+                return true;
+            }
+
+            UUID deniedRequesterId = tpaRequests.get(player.getUniqueId());
+            Player deniedRequester = Bukkit.getPlayer(deniedRequesterId);
+            if (deniedRequester != null) {
+                deniedRequester.sendMessage(ChatColor.RED + player.getName() + " hat deine Teleport-Anfrage abgelehnt.");
+            }
+
+            player.sendMessage(ChatColor.YELLOW + "Teleport-Anfrage abgelehnt.");
+            tpaRequests.remove(player.getUniqueId());
+            return true;
+        }
+
+        private boolean handleSpawnCommand(Player player) {
+            if (spawnLocation == null) {
+                player.sendMessage(ChatColor.RED + "Der Spawnpunkt wurde noch nicht gesetzt.");
+                return true;
+            }
+            player.teleport(spawnLocation);
+            player.sendMessage(ChatColor.GREEN + "Du wurdest zum Spawn teleportiert!");
+            return true;
+        }
+
+        private boolean handleSetSpawnCommand(Player player) {
+            spawnLocation = player.getLocation(); // Setzt den aktuellen Standort des Spielers als Spawnpunkt
+            player.sendMessage(ChatColor.GREEN + "Spawnpunkt gesetzt auf: " +
+                               "X: " + spawnLocation.getBlockX() +
+                               ", Y: " + spawnLocation.getBlockY() +
+                               ", Z: " + spawnLocation.getBlockZ());
             return true;
         }
 
